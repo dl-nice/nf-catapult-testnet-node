c97b07ff0e88ff7bcdc4eafc728b9086ccb733b8
Greg S <greg@evias.be>
2019-05-07 09:47:40 +0200

    latest addons to configuration script

--- testnet/generate-configurations.sh
+++ testnet/generate-configurations.sh
@@ -5,43 +5,108 @@ ADDRESSES_PATH=$1
 CONFIG_PATH=$2
 NODE_IP=${3:-api-node-0}
 
+CAT_BIN=`which cat`
+GREP_BIN=`which grep`
+TAIL_BIN=`which tail`
+SED_BIN=`which sed`
+AWK_BIN=`which awk`
+GIT_BIN=`which git`
+
+if [ -a ${ADDRESSES_PATH}/raw-addresses.txt ];
+then
+    echo 
+else
+    echo "[ERROR] Path '${ADDRESSES_PATH}' does not contain 'raw-addresses.txt' file."
+    exit 1
+fi
+
+# read private keys from build/generated-addresses/raw-addresses.txt
+read_private_key() {
+    local key=`${CAT_BIN} ${ADDRESSES_PATH}/raw-addresses.txt | ${GREP_BIN} -m $1 'private key:' | ${AWK_BIN} '{print $3}' | ${TAIL_BIN} -1`
+    echo "$key"
+}
+
+# read public keys from build/generated-addresses/raw-addresses.txt
+read_public_key() {
+    local key=`${CAT_BIN} ${ADDRESSES_PATH}/raw-addresses.txt | ${GREP_BIN} -m $1 'public key:' | ${AWK_BIN} '{print $3}' | ${TAIL_BIN} -1`
+    echo "$key"
+}
+
 # read private keys
-PRIVKEY_API_NODE=`/bin/cat ${ADDRESSES_PATH}/raw-addresses.txt | /bin/grep -m 1 'private key:' | /usr/bin/awk '{print $3}' | /usr/bin/tail -1`
-PRIVKEY_PEER_NODE=`/bin/cat ${ADDRESSES_PATH}/raw-addresses.txt | /bin/grep -m 2 'private key:' | /usr/bin/awk '{print $3}' | /usr/bin/tail -1`
-PRIVKEY_HARVEST=`/bin/cat ${ADDRESSES_PATH}/raw-addresses.txt | /bin/grep -m 3 'private key:' | /usr/bin/awk '{print $3}' | /usr/bin/tail -1`
-PRIVKEY_REST=`/bin/cat ${ADDRESSES_PATH}/raw-addresses.txt | /bin/grep -m 4 'private key:' | /usr/bin/awk '{print $3}' | /usr/bin/tail -1`
+PRIVKEY_API_NODE=$(read_private_key 1)
+PRIVKEY_PEER_NODE=$(read_private_key 2)
+PRIVKEY_HARVEST=$(read_private_key 3)
+PRIVKEY_REST=$(read_private_key 4)
 
 # read public keys
-PUBKEY_API_NODE=`/bin/cat ${ADDRESSES_PATH}/raw-addresses.txt | /bin/grep -m 1 'public key:' | /usr/bin/awk '{print $3}' | /usr/bin/tail -1`
-PUBKEY_PEER_NODE=`/bin/cat ${ADDRESSES_PATH}/raw-addresses.txt | /bin/grep -m 2 'public key:' | /usr/bin/awk '{print $3}' | /usr/bin/tail -1`
-PUBKEY_HARVEST=`/bin/cat ${ADDRESSES_PATH}/raw-addresses.txt | /bin/grep -m 3 'public key:' | /usr/bin/awk '{print $3}' | /usr/bin/tail -1`
-PUBKEY_REST=`/bin/cat ${ADDRESSES_PATH}/raw-addresses.txt | /bin/grep -m 4 'public key:' | /usr/bin/awk '{print $3}' | /usr/bin/tail -1`
+PUBKEY_API_NODE=$(read_public_key 1)
+PUBKEY_PEER_NODE=$(read_public_key 2)
+PUBKEY_HARVEST=$(read_public_key 3)
+PUBKEY_REST=$(read_public_key 4)
+
+# configure the api-node-0 API node
+config_api_node() {
+    ## 1) change NODE_IP if not local
+    ## 2) set bootKey
+    ## 3) register neighboor peer node
+    ## 4) register neighboor api node (self)
+    ${SED_BIN} -i -e "s/api-node-0/${NODE_IP}/" ${CONFIG_PATH}/api-node-0/userconfig/resources/config-node.properties
+    ${SED_BIN} -i -e "s/bootKey =.*/bootKey = ${PRIVKEY_API_NODE}/" ${CONFIG_PATH}/api-node-0/userconfig/resources/config-user.properties
+    ${SED_BIN} -i -e "s/\"publicKey\": \"\"/\"publicKey\": \"${PUBKEY_PEER_NODE}\"/" ${CONFIG_PATH}/api-node-0/userconfig/resources/peers-p2p.json
+    ${SED_BIN} -i -e "s/\"publicKey\": \"\"/\"publicKey\": \"${PUBKEY_API_NODE}\"/" ${CONFIG_PATH}/api-node-0/userconfig/resources/peers-api.json
+}
+
+# configure the peer-node-1 Peer node
+config_peer_node() {
+    ## 1) set harvestKey
+    ## 2) set bootKey
+    ## 3) register neighboor peer node (self)
+    ## 4) register neighboor api node
+    ${SED_BIN} -i -e "s/harvestKey =.*/harvestKey = ${PRIVKEY_HARVEST}/" ${CONFIG_PATH}/peer-node-1/userconfig/resources/config-harvesting.properties
+    ${SED_BIN} -i -e "s/bootKey =.*/bootKey = ${PRIVKEY_PEER_NODE}/" ${CONFIG_PATH}/peer-node-1/userconfig/resources/config-user.properties
+    ${SED_BIN} -i -e "s/\"publicKey\": \"\"/\"publicKey\": \"${PUBKEY_PEER_NODE}\"/" ${CONFIG_PATH}/peer-node-1/userconfig/resources/peers-p2p.json
+    ${SED_BIN} -i -e "s/\"publicKey\": \"\"/\"publicKey\": \"${PUBKEY_API_NODE}\"/" ${CONFIG_PATH}/peer-node-1/userconfig/resources/peers-api.json
+}
+
+# configure the rest-gateway-0 REST gateway
+config_rest_gateway() {
+    ## 1) set harvestKey (clientPrivateKey)
+    ## 2) set NODE_IP if not local
+    ## 3) register neighboor api node
+    ${SED_BIN} -i -e "s/\"clientPrivateKey\": \"\"/\"clientPrivateKey\": \"${PRIVKEY_HARVEST}\"/" ${CONFIG_PATH}/rest-gateway-0/userconfig/rest.json
+    ${SED_BIN} -i -e "s/api-node-0/${NODE_IP}/" ${CONFIG_PATH}/rest-gateway-0/userconfig/rest.json
+    ${SED_BIN} -i -e "s/\"publicKey\": \"\"/\"publicKey\": \"${PUBKEY_API_NODE}\"/" ${CONFIG_PATH}/rest-gateway-0/userconfig/rest.json
+}
+
+# save state with git
+save_state_with_git() {
+    ${GIT_BIN} checkout -b config-done
+    ${GIT_BIN} add build/
+    ${GIT_BIN} commit -m "configuration done"
+}
+
+echo Now configuring api-node-0..
+echo Using Private Key: ${PRIVKEY_API_NODE}
+echo
 
 # configure api-node-0
-## 1) change NODE_IP if not local
-## 2) set bootKey
-## 3) register neighboor peer node
-## 4) register neighboor api node (self)
-/bin/sed -i -e "s/api-node-0/${NODE_IP}/" ${CONFIG_PATH}/api-node-0/userconfig/resources/config-node.properties
-/bin/sed -i -e "s/bootKey =.*/bootKey = ${PRIVKEY_API_NODE}/" ${CONFIG_PATH}/api-node-0/userconfig/resources/config-user.properties
-/bin/sed -i -e "s/\"publicKey\": \"\"/\"publicKey\": \"${PUBKEY_PEER_NODE}\"/" ${CONFIG_PATH}/api-node-0/userconfig/resources/peers-p2p.json
-/bin/sed -i -e "s/\"publicKey\": \"\"/\"publicKey\": \"${PUBKEY_API_NODE}\"/" ${CONFIG_PATH}/api-node-0/userconfig/resources/peers-api.json
+config_api_node
+
+echo Now configuring peer-node-1..
+echo Using Private Key: ${PRIVKEY_PEER_NODE}
+echo
 
 # configure peer-node-1
-## 1) set harvestKey
-## 2) set bootKey
-## 3) register neighboor peer node (self)
-## 4) register neighboor api node
-/bin/sed -i -e "s/harvestKey =.*/harvestKey = ${PRIVKEY_HARVEST}/" ${CONFIG_PATH}/peer-node-1/userconfig/resources/config-harvesting.properties
-/bin/sed -i -e "s/bootKey =.*/bootKey = ${PRIVKEY_PEER_NODE}/" ${CONFIG_PATH}/peer-node-1/userconfig/resources/config-user.properties
-/bin/sed -i -e "s/\"publicKey\": \"\"/\"publicKey\": \"${PUBKEY_PEER_NODE}\"/" ${CONFIG_PATH}/peer-node-1/userconfig/resources/peers-p2p.json
-/bin/sed -i -e "s/\"publicKey\": \"\"/\"publicKey\": \"${PUBKEY_API_NODE}\"/" ${CONFIG_PATH}/peer-node-1/userconfig/resources/peers-api.json
+config_peer_node
+
+echo Now configuring rest-gateway-0..
+echo Using Private Key: ${PRIVKEY_REST}
+echo
 
 # configure rest-gateway-0
-## 1) set harvestKey (clientPrivateKey)
-## 2) set NODE_IP if not local
-## 3) register neighboor api node
-/bin/sed -i -e "s/\"clientPrivateKey\": \"\"/\"clientPrivateKey\": \"${PRIVKEY_HARVEST}\"/" ${CONFIG_PATH}/rest-gateway-0/userconfig/rest.json
-/bin/sed -i -e "s/api-node-0/${NODE_IP}/" ${CONFIG_PATH}/rest-gateway-0/userconfig/rest.json
-/bin/sed -i -e "s/\"publicKey\": \"\"/\"publicKey\": \"${PUBKEY_API_NODE}\"/" ${CONFIG_PATH}/rest-gateway-0/userconfig/rest.json
+config_rest_gateway
 
+# save configuration in git
+save_state_with_git
+
+echo Done configuring your Catapult Testnet node!
